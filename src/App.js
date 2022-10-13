1151c80efb8078e4af03af8ccb409d9735a511bb
Dev <gangwardev50@gmail.com>
2022-10-13 16:07:33 +0530

    header done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './Components/Header';
+import {BrowserRouter, Routes,Route} from "react-router-dom"
+import Card from './Components/Card';
+import CardData from './Components/CardData';
+
 
 function App() {
   return (
    <>
-
+   <Header/>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Card/>}/>
+      </Routes>
+    </BrowserRouter>
    </>
   );
 }
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,29 @@
 import React from 'react'
+import { Navbar } from 'react-bootstrap'
+import { Container, Nav } from 'react-bootstrap'
+import Badge from '@mui/material/Badge';
+import MailIcon from '@mui/icons-material/Mail';
+import { Link } from 'react-router-dom';
+
 
 export default function Header() {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <>
+
+            <Navbar expand="lg" variant="dark" bg="dark">
+                <Container>
+                    <Nav.Link to="/" className="text-decoration-none text-white">Add To Cart </Nav.Link>
+                    <Nav className="me-auto px-2">
+                        <Nav.Link>Home</Nav.Link>
+                    </Nav>
+
+                    <Badge badgeContent={4} color="primary">
+                        <MailIcon color="action" />
+                        <i className="fa fa-cart-plus text-light" style={{ fontSize: "26px", cursor: "pointer" }}></i>
+                    </Badge>
+                </Container>
+            </Navbar>
+
+        </>
+    )
 }
