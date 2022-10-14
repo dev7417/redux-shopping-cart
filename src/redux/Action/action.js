757d8b73a99533802cd1a5d3dc18b7a9553966a5
Dev <gangwardev50@gmail.com>
2022-10-14 16:06:40 +0530

    redux setup done

---
+++ src/redux/Action/action.js
@@ -0,0 +1,7 @@
+export const AddToCart = (item) =>{
+    return{
+
+        type:"ADD_TO_CART",
+        payload: item,
+    }
+}
---
+++ src/redux/reducer/main.js
@@ -0,0 +1,9 @@
+import { cartReducer } from "./reducer";
+import { combineReducers } from "redux";
+
+
+const rootReducer = combineReducers({
+    cartReducer
+})
+
+export default rootReducer
