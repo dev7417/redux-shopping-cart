43f563ec220ba2ad7bf6e968d0360ede6bc44846
Dev <gangwardev50@gmail.com>
2022-10-14 10:45:38 +0530

    populating data to our card component

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,24 +1,38 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import Cardsdata from './CardData';
+import { useState } from 'react';
+
 
 export default function Cards() {
+  const [data, setData] = useState(Cardsdata);
   return (
     <>
       <div className='container mt-3'>
         <h2 className='text-center'>Add to Cart Projects</h2>
         <div className='row'>
-          <Card style={{ width: '22rem', border:'none' }} className='mx-2 mt-4 card_style'>
-            <Card.Img variant="top" src="holder.js/100px180" style={{height:'16rem'}}/>
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                Some quick example text to build on the card title and make up the
-                bulk of the card's content.
-              </Card.Text>
-              <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-          </Card>
+          {
+            data.map((element, id) => {
+              console.log(element)
+              return (
+                <>
+                  <Card style={{ width: '22rem', border: 'none' }} className='mx-2 mt-4 card_style'>
+                    <Card.Img variant="top" src={element.imgdata}style={{ height: '16rem' }} />
+                    <Card.Body>
+                      <Card.Title>{element.rname}</Card.Title>
+                      <Card.Text>
+                        {element.address}
+                      </Card.Text>
+                      <Button variant="primary" className='d-flex mx-auto'>ADD</Button>
+                    </Card.Body>
+                  </Card>
+
+                </>
+              )
+            })
+          }
+
         </div>
       </div>
     </>
